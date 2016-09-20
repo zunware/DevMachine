5f7c2fc488a0414374a91e9bf4ac4e9bcdad8e03
Zunware <jose@josellausas.com>
2016-09-20 05:30:24 -0500

    Good so far

--- Vagrantfile
+++ Vagrantfile
@@ -18,6 +18,11 @@ Vagrant.configure("2") do |config|
   # boxes will only be checked for updates when the user runs
   # `vagrant box outdated`. This is not recommended.
   # config.vm.box_check_update = false
+  # 
+  #config.vm.provision "fix-no-tty", type: "shell" do |s|
+  #  s.privileged = false
+  #  s.inline = "sudo sed -i '/tty/!s/mesg n/tty -s \\&\\& mesg n/' /root/.profile"
+  #end
 
   # Create a forwarded port mapping which allows access to a specific port
   # within the machine from a port on the host machine. In the example below,
--- getReady.sh
+++ getReady.sh
@@ -1,3 +1,38 @@
+#!/usr/bin/env bash
 echo "===== Starting to get Ready ======"
 pwd
+export DEBIAN_FRONTEND=noninteractive
+
+# apt-get update
+# apt-get upgrade
+
+apt-get install -y git build-essential
+
+apt-get install -y nginx postgresql libpq-dev libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl
+
+mkdir devCode; cd devCode
+pwd
+
+# Download openresty
+echo "Downloading openresty"
+wget -q "https://openresty.org/download/openresty-1.11.2.1.tar.gz"
+echo "Done downloading openresty"
+
+# Extract and install
+sudo tar xvf "openresty-1.11.2.1.tar.gz"
+cd "openresty-1.11.2.1"
+./configure --with-pcre-jit --with-http_postgres_module --with-http_iconv_module
+make
+make install
+
+apt-get install -y zsh
+
+chsh -s $(which zsh) vagrant
+# Install oh-my-zsh
+sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Change the oh my zsh default theme.
+# sed -i 's/ZSH_THEME="robbyrussell"/ZSH_THEME="3den"/g' ~/.zshrc
+zsh
+
 echo "===== Done getting ready ====="
